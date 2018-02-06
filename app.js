20160eccc80c12ce94298ae3ded18ef01ec82ccf
crmgunter <crmgunter@gmail.com>
2018-02-06 18:56:12 -0500

    working on checker function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // else, subtract a life and add parts to hangman
 // same for all letters
 
-//when button is clicked, disable button
+//===when button is clicked, disable button====
 //button click = 1 try
 
 //when word is filled in completely, notify user of win
@@ -38,25 +38,52 @@ let splitAlphabet = function (letters) {
 splitAlphabet(alphabet)
 
 function checker(anything){
-console.log(anything)
+    let checkRandomWord = getRandomWord.split('')
+    for (let i = 0; i < checkRandomWord.length; i++){
+        if (anything === checkRandomWord[i]){
+            $('.answer-field').append(`${checkRandomWord[i]}`)
+        } else {
+            console.log("letter is NOT in this word x amount of times")
+        }
+    }
 }
+
+
+//this get the id of the button on click
+//thank you james, you sweet hunk
 $(".btn").click(function(catfood){
     let value = catfood.target.id
     checker(value)
+    matchLetters(value, getRandomWord)
+    console.log(value)
     $(`#${value}`).attr('disabled', true)
 })
+
+//this function gets the number of indexes for each letter
+
+let matchLetters = function (letters, word) {
+    for (i = 0; i < letters.length; i++) {
+        let count = 0
+        let eachIndex = word.indexOf(letters[i])
+        while (eachIndex !== -1) {
+            count++
+            eachIndex = word.indexOf(letters[i], eachIndex + 1)
+        }
+        console.log(count)
+    }
+}
+
 
 
 //split the random word into letters
 //not sure if needed at all
 
-// let splitRandomWord = function (words) {
-//     for (let i = 0; i < words.length; i++) {
-//         console.log(words[i])
-//         randomWordArray.push(words[i])
-//     }
-// }
-// splitRandomWord(getRandomWord)
+let splitRandomWord = function (words) {
+    for (let i = 0; i < words.length; i++) {
+        randomWordArray.push(words[i])
+    }
+}
+splitRandomWord(getRandomWord)
 
 
 
@@ -68,126 +95,26 @@ $(".btn").click(function(catfood){
 //track life
 //when a button is clicked, subtract one life
 //when lives is less than one, alert "you lose" and buttons are disabled
-let lifeTracker = function () {
-    $('.life-total').append(`${lives} lives remaining`)
-    $('.btn-disabled').on('click', function () {
-        lives -= 1
-        $('.life-total').html(`${lives} lives remaining`)
-        if (lives < 1) {
-            alert("you lose")
-            $('.btn-disabled').addClass('disabled')
-        }
-    })
-}
-lifeTracker()
-
-
-//this function gets the number of indexes for each letter
-let matchLetters = function (letters, word) {
-    for (i = 0; i < letters.length; i++) {
-        let singleLetter = letters[i]
-        console.log(letters[i])
-        let count = 0
-        let eachIndex = word.indexOf(letters[i])
-        while (eachIndex !== -1) {
-            count++
-            eachIndex = word.indexOf(letters[i], eachIndex + 1)
-        }
-        console.log(count)
-    }
-}
-matchLetters(alphabet, getRandomWord)
-
-
-//this is how to disable a button on click by class
-// now fucking figure out how to do it for all 26 letters with a for loop
-
-// $('.a').on('click', function () {
-//     $('.a').addClass('disabled')
+// let lifeTracker = function () {
+//     $('.life-total').append(`${lives} lives remaining`)
+//     $('.btn-disabled').on('click', function () {
+//         lives -= 1
+//         $('.life-total').html(`${lives} lives remaining`)
+//         if (lives < 1) {
+//             alert("you lose")
+//             $('.btn-disabled').addClass('disabled')
+//         }
 //     })
-// $('.b').on('click', function () {
-//     $('.b').addClass('disabled')
-// })
-// $('.c').on('click', function () {
-//     $('.c').addClass('disabled')
-// })
-// $('.d').on('click', function () {
-//     $('.d').addClass('disabled')
-// })
-// $('.e').on('click', function () {
-//     $('.e').addClass('disabled')
-//     let shit = getRandomWord.indexOf('e')
-//     console.log(shit)
-// })
-// $('.f').on('click', function () {
-//     $('.f').addClass('disabled')
-// })
-// $('.g').on('click', function () {
-//     $('.g').addClass('disabled')
-// })
-// $('.h').on('click', function () {
-//     $('.h').addClass('disabled')
-// })
-// $('.i').on('click', function () {
-//     $('.i').addClass('disabled')
-// })
-// $('.j').on('click', function () {
-//     $('.j').addClass('disabled')
-// })
-// $('.k').on('click', function () {
-//     $('.k').addClass('disabled')
-// })
-// $('.l').on('click', function () {
-//     $('.l').addClass('disabled')
-// })
-// $('.m').on('click', function () {
-//     $('.m').addClass('disabled')
-// })
-// $('.n').on('click', function () {
-//     $('.n').addClass('disabled')
-// })
-// $('.o').on('click', function () {
-//     $('.o').addClass('disabled')
-// })
-// $('.p').on('click', function () {
-//     $('.p').addClass('disabled')
-// })
-// $('.q').on('click', function () {
-//     $('.q').addClass('disabled')
-// })
-// $('.r').on('click', function () {
-//     $('.r').addClass('disabled')
-// })
-// $('.s').on('click', function () {
-//     $('.s').addClass('disabled')
-// })
-// $('.t').on('click', function () {
-//     $('.t').addClass('disabled')
-// })
-// $('.u').on('click', function () {
-//     $('.u').addClass('disabled')
-// })
-// $('.v').on('click', function () {
-//     $('.v').addClass('disabled')
-// })
-// $('.w').on('click', function () {
-//     $('.w').addClass('disabled')
-// })
-// $('.x').on('click', function () {
-//     $('.x').addClass('disabled')
-// })
-// $('.y').on('click', function () {
-//     $('.y').addClass('disabled')
-// })
-// $('.z').on('click', function () {
-//     $('.z').addClass('disabled')
-// })
+// }
+// lifeTracker()
 
 
-//reset button
-const resetGame = function () {
-}
-resetGame()
+
+
+// //reset button
+// const resetGame = function () {
+// }
+// resetGame()
 
 //where i left off: buttons need troubleshooting
 //reset button does nothing when clicked
