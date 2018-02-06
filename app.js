9bca91d5ecc170871b27df3e06bd3d76b0bf2fc0
crmgunter <crmgunter@gmail.com>
2018-02-05 19:22:05 -0500

    added buttons and random word generator

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-alert("javascript working")
+//======get random word========
+//if "A" is picked, search word for "A"
+// if word contains "A", fill in "A" in all spots it belongs
+// else, subtract a life and add parts to hangman
+// same for all letters
+
+//when button is clicked, disable button
+//button click = 1 try
+
+//when word is filled in completely, notify user of win
+//when run out of tries, you lose
+
+
+//words to be guessed by user
+const secretWords = ["banana", "frog", "bazooka"]
+let getRandomWord = secretWords[Math.floor(Math.random() * secretWords.length)]
+console.log(getRandomWord)
+
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+//split alphabet string and push to alphabet array
+let splitAlphabet = function (letters){
+    let alphabetArray = []
+    console.log(alphabetArray)
+    for(let i = 0; i < letters.length; i++){
+        alphabetArray.push(letters[i])
+        let toButtonForm = letters[i]
+        $('.letters-container').append(`<button class="btn red button-style">${toButtonForm}</button>`)
+    }
+}
+splitAlphabet(alphabet)
+
+
+//split the random word into letters
+let splitRandomWord = function (words){
+    randomWordArray = []
+    console.log(randomWordArray)
+    for(let i = 0; i < words.length; i++){
+        console.log(words[i])
+        randomWordArray.push(words[i])
+    }
+}
+splitRandomWord(getRandomWord)
+
+
+// answer section with spaces for blank letters
+
+let answerSection = function () {
+    let printRandomWord = $('.answer-field').html(`${getRandomWord}`)
+}
+
+answerSection()
