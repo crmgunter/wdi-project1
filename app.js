761ed68466d83ee7e44c12c05175a52579a5b3eb
crmgunter <crmgunter@gmail.com>
2018-02-06 13:15:02 -0500

    created logic to check index of words/letters

--- app.js
+++ app.js
@@ -12,31 +12,35 @@
 
 
 //words to be guessed by user
-const secretWords = ["banana", "frog", "bazooka", "boomerang", "seashells", "boobies", "mailbox", "yesterday"]
+const secretWords = ["banana", "frog", "bazooka", "boomerang", "seashells", "cheese", "abcdefghijklmnopqrstuvwxyz", "mailbox", "yesterday", "leader", "pompous", "friendly", "jello", "personalize"]
 let getRandomWord = secretWords[Math.floor(Math.random() * secretWords.length)]
 console.log(getRandomWord)
+// answer section with spaces for blank letters
+let printRandomWord = $('.answer-field').html(`${getRandomWord}`)
 
 let lives = 6
 let alphabet = "abcdefghijklmnopqrstuvwxyz"
+//prints alphabet string into an array
+let alphabetArray = []
+console.log(alphabetArray)
+
+//prints random word into array
+randomWordArray = []
+    console.log(randomWordArray)
 
 //split alphabet string and push to alphabet array
-let splitAlphabet = function (letters){
-    let alphabetArray = []
-    console.log(alphabetArray)
-    for(let i = 0; i < letters.length; i++){
+let splitAlphabet = function (letters) {
+    for (let i = 0; i < letters.length; i++) {
         alphabetArray.push(letters[i])
-        let toButtonForm = letters[i]
-        let pushClassOut = $('.letters-container').append(`<button class="btn btn-disabled ${letters[i]} red button-style">${toButtonForm}</button>`)
+        $('.letters-container').append(`<button class="btn btn-disabled ${letters[i]} red button-style">${letters[i]}</button>`)
     }
 }
 splitAlphabet(alphabet)
 
 
 //split the random word into letters
-let splitRandomWord = function (words){
-    randomWordArray = []
-    console.log(randomWordArray)
-    for(let i = 0; i < words.length; i++){
+let splitRandomWord = function (words) {
+    for (let i = 0; i < words.length; i++) {
         console.log(words[i])
         randomWordArray.push(words[i])
     }
@@ -44,118 +48,133 @@ let splitRandomWord = function (words){
 splitRandomWord(getRandomWord)
 
 
-// answer section with spaces for blank letters
 
-let answerSection = function () {
-    let printRandomWord = $('.answer-field').html(`${getRandomWord}`)
-}
 
-answerSection()
+
 
 
 
 //track life
 //when a button is clicked, subtract one life
 //when lives is less than one, alert "you lose" and buttons are disabled
-let lifeTracker = function(){
-$('.life-total').append(`${lives} lives remaining`)
-$('.btn-disabled').on('click', function(){
-    lives -= 1
-    $('.life-total').html(`${lives} lives remaining`)
-    if (lives < 1){
-        alert("you lose")
-        $('.btn-disabled').addClass('disabled')
-    }
-})
+let lifeTracker = function () {
+    $('.life-total').append(`${lives} lives remaining`)
+    $('.btn-disabled').on('click', function () {
+        lives -= 1
+        $('.life-total').html(`${lives} lives remaining`)
+        if (lives < 1) {
+            alert("you lose")
+            $('.btn-disabled').addClass('disabled')
+        }
+    })
 }
 lifeTracker()
+
+
+//this function gets the number of indexes for each letter
+let matchLetters = function (letters, word) {
+    for (i = 0; i < letters.length; i++) {
+        let singleLetter = letters[i]
+        console.log(letters[i])
+        let count = 0
+        let eachIndex = word.indexOf(letters[i])
+        while (eachIndex !== -1) {
+            count++
+            eachIndex = word.indexOf(letters[i], eachIndex + 1)
+        }
+        console.log(count)
+    }
+}
+matchLetters(alphabet, getRandomWord)
+
+
 
 
 
 //this is how to disable a button on click by class
 // now fucking figure out how to do it for all 26 letters with a for loop
-$('.a').on('click', function(){
+$('.a').on('click', function () {
     $('.a').addClass('disabled')
 })
-$('.b').on('click', function(){
+$('.b').on('click', function () {
     $('.b').addClass('disabled')
 })
-$('.c').on('click', function(){
+$('.c').on('click', function () {
     $('.c').addClass('disabled')
 })
-$('.d').on('click', function(){
+$('.d').on('click', function () {
     $('.d').addClass('disabled')
 })
-$('.e').on('click', function(){
+$('.e').on('click', function () {
     $('.e').addClass('disabled')
 })
-$('.f').on('click', function(){
+$('.f').on('click', function () {
     $('.f').addClass('disabled')
 })
-$('.g').on('click', function(){
+$('.g').on('click', function () {
     $('.g').addClass('disabled')
 })
-$('.h').on('click', function(){
+$('.h').on('click', function () {
     $('.h').addClass('disabled')
 })
-$('.i').on('click', function(){
+$('.i').on('click', function () {
     $('.i').addClass('disabled')
 })
-$('.j').on('click', function(){
+$('.j').on('click', function () {
     $('.j').addClass('disabled')
 })
-$('.k').on('click', function(){
+$('.k').on('click', function () {
     $('.k').addClass('disabled')
 })
-$('.l').on('click', function(){
+$('.l').on('click', function () {
     $('.l').addClass('disabled')
 })
-$('.m').on('click', function(){
+$('.m').on('click', function () {
     $('.m').addClass('disabled')
 })
-$('.n').on('click', function(){
+$('.n').on('click', function () {
     $('.n').addClass('disabled')
 })
-$('.o').on('click', function(){
+$('.o').on('click', function () {
     $('.o').addClass('disabled')
 })
-$('.p').on('click', function(){
+$('.p').on('click', function () {
     $('.p').addClass('disabled')
 })
-$('.q').on('click', function(){
+$('.q').on('click', function () {
     $('.q').addClass('disabled')
 })
-$('.r').on('click', function(){
+$('.r').on('click', function () {
     $('.r').addClass('disabled')
 })
-$('.s').on('click', function(){
+$('.s').on('click', function () {
     $('.s').addClass('disabled')
 })
-$('.t').on('click', function(){
+$('.t').on('click', function () {
     $('.t').addClass('disabled')
 })
-$('.u').on('click', function(){
+$('.u').on('click', function () {
     $('.u').addClass('disabled')
 })
-$('.v').on('click', function(){
+$('.v').on('click', function () {
     $('.v').addClass('disabled')
 })
-$('.w').on('click', function(){
+$('.w').on('click', function () {
     $('.w').addClass('disabled')
 })
-$('.x').on('click', function(){
+$('.x').on('click', function () {
     $('.x').addClass('disabled')
 })
-$('.y').on('click', function(){
+$('.y').on('click', function () {
     $('.y').addClass('disabled')
 })
-$('.z').on('click', function(){
+$('.z').on('click', function () {
     $('.z').addClass('disabled')
 })
 
 
 //reset button
-const resetGame = function(){
+const resetGame = function () {
 }
 resetGame()
 
